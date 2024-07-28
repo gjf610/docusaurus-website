46a42d608b4aa34fe0464044da80e6eae1d05ca6
jianfeng <gjf610@yeah.net>
2024-07-29 04:25:56 +0800

    fix: baseUrl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://docs.qiueyeqiu.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -86,7 +86,7 @@ const config: Config = {
           items: [
             {
               label: 'Tutorial',
-              to: '/docs/docs/intro',
+              to: '/docs/intro',
             },
           ],
         },
